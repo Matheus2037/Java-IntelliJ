51ebff208866199a851367d79a8bac5b603ac7b0
Matheus Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2023-08-27 21:14:02 -0300

    Segundo teste
    Criando uma soma simples e testando a formatação de Strings no java

---
+++ src/SegundoTeste.java
@@ -0,0 +1,13 @@
+public class SegundoTeste {
+
+    public static void main(String[] args){
+        int nr1 = 2;
+        int nr2 = 2;
+        int soma = nr1 + nr2;
+        String mensagemFormatada = String.format("O resultado é de %s", soma);
+
+        System.out.println(mensagemFormatada);
+
+    }
+
+}
