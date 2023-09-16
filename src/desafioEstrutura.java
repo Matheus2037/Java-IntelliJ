e4b27f1695113cb37b7ef8fa119ec09c8e13b406
Matheus Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2023-09-16 10:14:22 -0300

    Update desafioEstrutura.java
    Finalização do desafio, a função de adicionar no ultimo lugar foi ajeitada, foi substituido o break de todos os casos, foi criado no final do código a finalização do mesmo, foi criado a opção de excluir tanto a ultima posição do vetor, ou selecionar a posição que deseja excluir

--- src/desafioEstrutura.java
+++ src/desafioEstrutura.java
@@ -2,8 +2,7 @@ import javax.swing.*;
 import java.util.Arrays;
 
 public class desafioEstrutura {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         double[] nInteiros = new double[7];
         int loop = 1;
 
@@ -15,45 +14,50 @@ public class desafioEstrutura {
                     4 - Imprimir
                     0 - Sair"""));
 
-            switch (escolha){
-
-                case 1:
-                    int modoInserir = Integer.parseInt(JOptionPane.showInputDialog("1 - Ultimo lugar"+
+            switch (escolha) {
+                case 1 -> {
+                    int modoInserir = Integer.parseInt(JOptionPane.showInputDialog("1 - Ultimo lugar" +
                             "\n2 - Escolher posição"));
-                    if(modoInserir == 1){
+                    if (modoInserir == 1) {
                         int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número"));
-                        for (int i = nInteiros.length - 1; i > 0; i--) {
-                            nInteiros[i] = nInteiros[i - 1];
+                        for (int i = 0; i < nInteiros.length - 1; i++) {
+                            nInteiros[i] = nInteiros[i + 1];
                         }
-                        nInteiros[0] = n;
-                    }else if(modoInserir == 2) {
+                        nInteiros[6] = n;
+                    } else if (modoInserir == 2) {
                         int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número: "));
                         int posicao = Integer.parseInt(JOptionPane.showInputDialog("Digite a posição: "));
                         nInteiros[posicao] = n;
                     }
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número"));
-                    for (int i=0; i<nInteiros.length; i++){
-                        if (n == nInteiros[i]){
-                            JOptionPane.showMessageDialog(null,"O número "+n+" está na lista");
+                    for (int i = 0; i < nInteiros.length; i++) {
+                        if (n == nInteiros[i]) {
+                            JOptionPane.showMessageDialog(null, "O número " + n + " está na lista");
                             i = nInteiros.length;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"O número "+n+" não está na lista");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "O número " + n + " não está na lista");
                         }
                     }
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-                    System.out.println(Arrays.toString(nInteiros));
-                    break;
-                case 0:
-                    loop = 0;
-                    break;
+                }
+                case 3 -> {
+                    int modoExcluir = Integer.parseInt(JOptionPane.showInputDialog("1 - Ultimo lugar" +
+                            "\n2 - Escolher posição"));
+                    if (modoExcluir == 1) {
+                        nInteiros[6] = 0;
+                        for (int i = nInteiros.length - 1; i > 0; i--) {
+                            nInteiros[i] = nInteiros[i - 1];
+                        }
+                    } else if (modoExcluir == 2) {
+                        int posicao = Integer.parseInt(JOptionPane.showInputDialog("Digite a posição: "));
+                        nInteiros[posicao] = 0;
+                    }
+                }
+                case 4 -> System.out.println(Arrays.toString(nInteiros));
+                case 0 -> loop = 0;
             }
         }
+        System.exit(0);
     }
 }
